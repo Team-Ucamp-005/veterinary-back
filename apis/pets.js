e5c2bedd8fa5a13c3248176e72c76100b9698bc9
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-07-02 11:56:28 -0500

    CRUD

--- apis/pets.js
+++ apis/pets.js
@@ -3,11 +3,55 @@ const mongoose = require('mongoose')
 const router = express.Router()
 const { petsController } = require('../controllers')
 
-const { getPets } = petsController
+const { getPets, createPet, updatePet, deletePet } = petsController
 
 router.get('/', async (req, res) => {
   const pets = await getPets()
   res.send(pets)
+})
+
+router.post('/', async (req, res) => {
+  const body = req.body
+
+  try {
+    const newPet = await createPet(body)
+    res.status(201)
+    res.send(newPet)
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      res.status(400)
+      return res.send({
+        message: 'Error de validación',
+        reason: err.message
+      })
+    }
+    res.status(500)
+    return res.send({
+      error: err.message
+    })
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const body = req.body
+  const pet = await updatePet(id, body)
+
+  if (!pet) {
+    res.status(404)
+    return res.send({
+      message: `La mascota con id: ${id}, no se encuentra`
+    })
+  }
+  res.send(pet)
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+
+  const result = await deletePet(id)
+
+  res.send(result)
 })
 
 module.exports = router
--- controllers/petsController.js
+++ controllers/petsController.js
@@ -4,6 +4,26 @@ const getPets = async () => {
   return PetModel.find({})
 }
 
+const createPet = async (body) => {
+  const newPet = new PetModel(body)
+  newPet.save()
+  return newPet
+}
+
+const updatePet = async (_id, updateObject) => {
+  return PetModel.findOneAndUpdate({ _id }, updateObject, {
+    upsert: false,
+    new: true
+  })
+}
+
+const deletePet = async (_id) => {
+  return PetModel.findOneAndDelete({ _id })
+}
+
 module.exports = {
-  getPets
+  getPets,
+  createPet,
+  updatePet,
+  deletePet
 }
